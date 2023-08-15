a0c79143965a94aadd9a62252d44a8ec4079010f
XSam28 <89696400+XSam28@users.noreply.github.com>
2023-08-15 10:56:18 +0700

    Update getSpace.jsx

--- app/libs/getSpace.jsx
+++ app/libs/getSpace.jsx
@@ -1,5 +1,5 @@
 export default async function getSpace() {
-    const res = await fetch("https://api.nasa.gov/planetary/apod?api_key=CRKl91Lv8vhE8vhgoYVgnfLbTCwzCtfytoQwnnrY&count=2")
+    const res = await fetch("https://api.nasa.gov/planetary/apod?api_key=CRKl91Lv8vhE8vhgoYVgnfLbTCwzCtfytoQwnnrY&count=30")
     if(!res.ok){
         throw new Error('Failed to fetch data. refers this page')
     }
