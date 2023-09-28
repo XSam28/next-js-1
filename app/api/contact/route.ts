afccc577af386bcd7e5397f0c9fdcb6357d4067e
XSam28 <petrasamuel645@gmail.com>
2023-09-28 22:21:10 +0700

    fix

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,19 +1,18 @@
 import { NextResponse } from "next/server";
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function POST(req:Request){
-  const { name, email, message } = await req.json()
+export async function POST(req: Request) {
+  const { name, email, message } = await req.json();
 
   const user = await prisma.user.create({
     data: {
       name,
       email,
-      message
+      message,
     },
   });
-  
-    return NextResponse.json({message: "successfull sent", user})
 
+  return NextResponse.json({ message: "successfull sent", user });
 }
